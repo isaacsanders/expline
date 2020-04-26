e065a9435366d1b0b68af414617d4e0fbc72a0b5
Aleksei Matiushkin <aleksei.matiushkin@kantox.com>
2020-04-26 11:56:25 +0200

    Modern era update

--- lib/expline/vector.ex
+++ lib/expline/vector.ex
@@ -4,28 +4,26 @@ defmodule Expline.Vector do
   @enforce_keys [:n_slots, :internal]
   defstruct [:n_slots, :internal]
 
-  @type t() :: %Expline.Vector{ n_slots: pos_integer(),
-                                internal: internal() }
+  @type t() :: %Expline.Vector{n_slots: pos_integer(), internal: internal()}
   @typep internal() :: tuple()
 
   @spec construct(pos_integer(), (non_neg_integer() -> float())) :: Expline.Vector.t()
   def construct(n_slots, elem_fn)
-  when n_slots > 0
-  and is_function(elem_fn, 1) do
-    internal = 0..(n_slots - 1)
-    |> Enum.reduce({}, fn (i, vec) ->
-      Tuple.append(vec, elem_fn.(i))
-    end)
+      when n_slots > 0 and
+             is_function(elem_fn, 1) do
+    internal =
+      Enum.reduce(0..(n_slots - 1), {}, fn i, vec ->
+        Tuple.append(vec, elem_fn.(i))
+      end)
 
-    %Expline.Vector{ n_slots: n_slots, internal: internal }
+    %Expline.Vector{n_slots: n_slots, internal: internal}
   end
 
   @spec at(Expline.Vector.t(), non_neg_integer()) :: float()
-  def at(%Expline.Vector{ n_slots: n_slots, internal: internal }, i)
-  when is_integer(i)
-  and i < n_slots do
-    internal
-    |> elem(i)
+  def at(%Expline.Vector{n_slots: n_slots, internal: internal}, i)
+      when is_integer(i) and
+             i < n_slots do
+    elem(internal, i)
   end
 
   def to_list(%Expline.Vector{} = vector) do
--- mix.exs
+++ mix.exs
@@ -1,25 +1,30 @@
 defmodule Expline.Mixfile do
   use Mix.Project
 
+  @app :expline
+  @version "0.2.0"
+
   def project do
-    [app: :expline,
-     version: "0.1.0",
-     elixir: "~> 1.4",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps(),
+    [
+      app: @app,
+      version: @version,
+      elixir: "~> 1.8",
+      build_embedded: Mix.env() == :prod,
+      start_permanent: Mix.env() == :prod,
+      deps: deps(),
 
-     # Hex parameters
-     description: description(),
-     package: package(),
+      # Hex parameters
+      description: description(),
+      package: package(),
 
-     # Type-checking flags
-     dialyzer: dialyzer(),
+      # Type-checking flags
+      dialyzer: dialyzer(),
 
-     # Generated documentation parameters
-     name: "Expline",
-     source_url: "https://github.com/isaacsanders/expline",
-     docs: docs()]
+      # Generated documentation parameters
+      name: "Expline",
+      source_url: "https://github.com/isaacsanders/#{@app}",
+      docs: docs()
+    ]
   end
 
   def application do
@@ -36,28 +41,46 @@ defmodule Expline.Mixfile do
   end
 
   def package do
-    [name: :expline,
-     files: ["lib", "mix.exs", "README*", "LICENSE*"],
-     licenses: ["MIT"],
-     maintainers: ["Isaac Sanders"],
-     links: %{"GitHub" => "https://github.com/isaacsanders/expline"}]
+    [
+      name: :expline,
+      files: ["lib", "mix.exs", "README*", "LICENSE*"],
+      licenses: ["MIT"],
+      maintainers: ["Isaac Sanders"],
+      links: %{"GitHub" => "https://github.com/isaacsanders/#{@app}"}
+    ]
   end
 
   def dialyzer do
-    [flags: [:error_handling, :no_behaviours, :no_contracts, :no_fail_call,
-      :no_fun_app, :no_improper_lists, :no_match, :no_missing_calls, :no_opaque,
-      :no_return, :no_undefined_callbacks, :no_unused, :race_conditions,
-      :underspecs, :unknown]]
+    [
+      flags: [
+        :error_handling,
+        :no_behaviours,
+        :no_contracts,
+        :no_fail_call,
+        :no_fun_app,
+        :no_improper_lists,
+        :no_match,
+        :no_missing_calls,
+        :no_opaque,
+        :no_return,
+        :no_undefined_callbacks,
+        :no_unused,
+        :race_conditions,
+        :underspecs,
+        :unknown
+      ]
+    ]
   end
 
   def docs do
-    [main: "Expline",
-     extras: ["README.md"]]
+    [main: "Expline", extras: ["README.md"]]
   end
 
   defp deps do
-    [{:ex_doc,   "~> 0.14", only: :dev, runtime: false},
-     {:dialyxir, "~> 0.5",  only: :dev, runtime: false},
-     {:quixir,   "~> 0.9",  only: :test }]
+    [
+      {:ex_doc, "~> 0.14", only: :dev, runtime: false},
+      {:dialyxir, "~> 1.0", only: :dev, runtime: false},
+      {:quixir, "~> 0.9", only: :test}
+    ]
   end
 end
